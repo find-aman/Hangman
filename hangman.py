432bd254937d5ac357550eeebe5ac4ca60d5be3c
Aman Gupta <amang9578@gmail.com>
2020-05-24 13:18:20 +0530

    further improvements

--- hangman.py
+++ hangman.py
@@ -2,32 +2,42 @@ import random
 print("H A N G M A N")
 word_list = ['python', 'java', 'kotlin', 'javascript']
 word = random.choice(word_list)
-check_word = word
 word_in_list = list(word)
 empty_list = list("-" * len(word))
 count = 0
 ans = ""
+already_entered_list = list()
 while count < 8:
     print()
     print("".join(empty_list))
     input_letter = input("Input a letter:")
     count1 = 0
-    if input_letter in word_in_list:
-        if input_letter in empty_list:
-            print("No improvements")
-            count += 1
+    if len(input_letter) == 1:
+        if ord(input_letter) >= 97 and ord(input_letter) <= 122:
+            if input_letter in already_entered_list:
+                print("You already typed this letter")
+            else:
+                already_entered_list.append(input_letter)
+                if input_letter in word_in_list:
+                    if input_letter in empty_list:
+                        print("You already typed this letter")
+                    else:
+                        for i in word_in_list:
+                            count1 += 1
+                            if i == input_letter:
+                                empty_list[count1 - 1] = input_letter
+                        ans = "".join(empty_list)
+                else:
+                    print("No such letter in the word")
+                    count += 1
+                if ans == word:
+                    print(ans)
+                    break
         else:
-            for i in word_in_list:
-                count1 += 1
-                if i == input_letter:
-                    empty_list[count1-1] = input_letter
-            ans = "".join(empty_list)
+            print("It is not an ASCII lowercase letter")
     else:
-        print("No such letter in the word")
-        count += 1
-    if ans == word:
-        print(ans)
-        break
+        print("You should input a single letter")
+
 if word == "".join(empty_list):
     print("You guessed the word!")
     print("You survived!")
